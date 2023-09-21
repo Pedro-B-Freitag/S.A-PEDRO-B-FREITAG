4476621277618a1701c45e622555e2d40ee5001a
Pedro-B-Freitag <pedrobosini09@icloud.com>
2023-09-21 16:53:42 -0300

    .

--- cursos/cursos/src/main/java/com/cursos/modulos/curso/Pessoa/DAOs/PessoaDAO.java
+++ cursos/cursos/src/main/java/com/cursos/modulos/curso/Pessoa/DAOs/PessoaDAO.java
@@ -1,13 +1,12 @@
 package com.cursos.modulos.curso.Pessoa.DAOs;
 
-import com.cursos.modulos.curso.Empresa.Empresa;
 import com.cursos.modulos.curso.Pessoa.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 
 import java.util.List;
 
 public interface PessoaDAO {
-
     List<Pessoa> findAll();
     Pessoa findById(int oId);
     Pessoa save(Pessoa aPessoa);
--- cursos/cursos/src/main/java/com/cursos/security/SecurityConfig.java
+++ cursos/cursos/src/main/java/com/cursos/security/SecurityConfig.java
@@ -1,23 +1,20 @@
 package com.cursos.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+
+
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
@@ -39,6 +36,7 @@ public class SecurityConfig {
                 http.authorizeHttpRequests(configurer ->
                 configurer
                         .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .requestMatchers("/home").permitAll()
                         .anyRequest().authenticated()
                         )
 
