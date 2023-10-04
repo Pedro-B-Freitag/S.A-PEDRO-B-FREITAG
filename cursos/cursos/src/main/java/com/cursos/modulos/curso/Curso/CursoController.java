93ff217e18e1f952f4829fc1af2c16c9965345e0
Pedro-B-Freitag <pedrobosini09@icloud.com>
2023-10-03 23:13:07 -0300

    .

--- cursos/cursos/src/main/java/com/cursos/modulos/curso/Curso/CursoController.java
+++ cursos/cursos/src/main/java/com/cursos/modulos/curso/Curso/CursoController.java
@@ -2,14 +2,10 @@ package com.cursos.modulos.curso.Curso;
 
 
 import com.cursos.modulos.curso.Curso.Services.CursoService;
-import org.apache.catalina.Engine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -46,13 +42,9 @@ public class CursoController {
     }
 
     @PostMapping("/save")
-    public String saveCurso(@ModelAttribute("curso") Curso oCurso,@RequestParam("fileCurso") MultipartFile file)  {
+    public String saveCurso(@ModelAttribute("curso") Curso oCurso)  {
         cursoService.save(oCurso);
-        try {
-            oCurso.setImagem(String.valueOf(file.getBytes()));
-        } catch (IOException e) {
-                e.printStackTrace();
-        }
+
         return "redirect:/cursos/list";
     }
 
