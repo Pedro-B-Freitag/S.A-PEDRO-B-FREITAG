8aadd72a63b388cb06f70860b78c2fce7ca2f5f6
Pedro Bosini Freitag <127051349+Pedro-B-Freitag@users.noreply.github.com>
2023-12-14 18:09:45 -0300

    Delete cursos directory

--- cursos/cursos/src/main/java/com/cursos/modulos/curso/Usuario/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cursos.modulos.curso.Usuario;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Usuario, Integer> {
-}
